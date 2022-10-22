255efa04b870c948c960aca5a9719731846db4b1
Shaked Dotan <shaked.dotan@develeap.com>
2022-10-22 21:09:26 +0300

    working readable num

--- learning_python/readable_num/readable_num.py
+++ learning_python/readable_num/readable_num.py
@@ -1,16 +1,30 @@
 global N; N = 3
 
-def readable_num(num):
+# 123   --> 123
+# 1234  --> 1,234
+# 12345 --> 12,345
+
+def readable_num_1(num):
     num = str(num)
     length = len(num)
-    Res = ""
-    # List = [ [num[i:i+N]] for i in range(0,len(num),N) ]
+    res = ''
 
-    sub_list = [ [num[-i-N:length-i]] for i in range(0,len(num),N) ]
+    sub_tuple = (num[-i-N:length-i] for i in range(0,len(num),N))
+    for sub in sub_tuple:
+        res = f"{sub},{res}"
+    return res[:-1]
 
-    print(','.join(sub_list))
-    # for sub in List:
-        # Res += List[len(List)-x-1][0] + ","
-    return Res[:-1]
 
-print(readable_num(12345676576532658388))
+def readable_num_2(num):
+    num = str(num)
+    res = ''
+
+    for i in range(0,len(num),N):
+        res = num[-i-N:len(num)-i] + ',' + res
+    return res[:-1]
+
+
+print(readable_num_2(123))
+print(readable_num_2(12345))
+print(readable_num_2(123456))
+print(readable_num_2(1234567))
